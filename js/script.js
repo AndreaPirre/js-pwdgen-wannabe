0c23b88bb712a9ab2145ca702dbe73be98c6d28c
Andrea <andreapirrello1990@gmail.com>
2020-06-12 14:16:50 +0200

    generatore password

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// chiedere all'utente il suo nome
+var nome = prompt("inserisci il tuo nome:");
+
+// chiedere all'utente il suo cognome
+var cognome = prompt("inserisci il tuo cognome:");
+
+// chiedere all'utente il suo colore preferito
+var colore = prompt("inserisci il tuo colore preferito:");
+
+// calcolo password
+var password =nome + cognome + colore;
+
+// scriverE sulla pagina nomecognomecolorepreferito19
+
+document.getElementById('psw').innerHTML = "La tua nuova password generata: " + password + 19;
